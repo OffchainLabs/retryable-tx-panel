3ced817cacc5f0bb6b7034463572d074699f7fb4
gzeon <95478735+gzeoneth@users.noreply.github.com>
2022-03-30 22:49:30 +0800

    local dev network

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,8 @@ import {
   L1Network,
   L2Network,
   getL2Network,
-  getL1Network
+  getL1Network,
+  addCustomNetwork
 } from "@arbitrum/sdk"
 
 import { JsonRpcProvider } from "@ethersproject/providers";
@@ -115,13 +116,19 @@ export interface RetryableTxs {
   l2ChainId: number;
 }
 
-if (!process.env.REACT_APP_INFURA_KEY)
-  throw new Error("No REACT_APP_INFURA_KEY set");
+// if (!process.env.REACT_APP_INFURA_KEY)
+//   throw new Error("No REACT_APP_INFURA_KEY set");
 
 const supportedL1Networks = {
-  1: `https://mainnet.infura.io/v3/${process.env.REACT_APP_INFURA_KEY}`,
-  4: `https://rinkeby.infura.io/v3/${process.env.REACT_APP_INFURA_KEY}`
+  // 1: `https://mainnet.infura.io/v3/${process.env.REACT_APP_INFURA_KEY}`,
+  // 4: `https://rinkeby.infura.io/v3/${process.env.REACT_APP_INFURA_KEY}`,
+  1337: `http://localhost:8545`
 };
+
+addCustomNetwork({
+  customL1Network: {"blockTime":10,"chainID":1337,"explorerUrl":"","isCustom":true,"name":"EthLocal","partnerChainIDs":[421612],"rpcURL":"http://localhost:8545"},
+  customL2Network: {"chainID":421612,"confirmPeriodBlocks":20,"ethBridge":{"bridge":"0x9b284e41921fb974a26f9abe66e54f56e406b730","inbox":"0x30fc4c5f7973ed68e9c858fcbc4a30ea7fc72967","outboxes":{"0xCC17370c3ae15C598f6575Fa0105f3A70b626A68":0},"rollup":"0xac08e3b85b23b87835640c4799ac160a36655c2c","sequencerInbox":"0xbb665b5f0e66accb90f37ebc2d6b84b9c9ab8702"},"explorerUrl":"http://localhost:4000","isArbitrum":true,"isCustom":true,"name":"ArbLocal","partnerChainID":1337,"rpcURL":"http://localhost:7545","tokenBridge":{"l1CustomGateway":"0xDe67138B609Fbca38FcC2673Bbc5E33d26C5B584","l1ERC20Gateway":"0x0Bdb0992B3872DF911260BfB60D72607eb22d5d4","l1GatewayRouter":"0x4535771b8D5C43100f126EdACfEc7eb60d391312","l1MultiCall":"0x36BeF5fD671f2aA8686023dE4797A7dae3082D5F","l1ProxyAdmin":"0xF7818cd5f5Dc379965fD1C66b36C0C4D788E7cDB","l1Weth":"0x24067223381F042fF36fb87818196dB4D2C56E9B","l1WethGateway":"0xBa3d12E370a4b592AAF0CA1EF09971D196c27aAd","l2CustomGateway":"0xF0B003F9247f2DC0e874710eD55e55f8C63B14a3","l2ERC20Gateway":"0x78a6dC8D17027992230c112432E42EC3d6838d74","l2GatewayRouter":"0x7b650845242a96595f3a9766D4e8e5ab0887936A","l2Multicall":"0x9b890cA9dE3D317b165afA7DFb8C65f2e4c95C20","l2ProxyAdmin":"0x7F85fB7f42A0c0D40431cc0f7DFDf88be6495e67","l2Weth":"0x36BeF5fD671f2aA8686023dE4797A7dae3082D5F","l2WethGateway":"0x2E76efCC2518CB801E5340d5f140B1c1911b4F4B"}},
+})
 
 const getL1TxnReceipt = async (txnHash: string) => {
   for (let [chainID, rpcURL] of Object.entries(supportedL1Networks)) {
