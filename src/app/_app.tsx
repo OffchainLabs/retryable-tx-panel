f879defef8e2564f2741420aa06bcebc3aafcb71
Christophe <contact@christophedeveaux.com>
2023-03-20 11:12:41 +0100

    Add fonts

--- src/app/_app.tsx
+++ src/app/_app.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
-// import { Saira, DM_Sans } from 'next/font/google';
+import { Saira, DM_Sans } from 'next/font/google';
 import Head from 'next/head';
 import type { AppProps } from 'next/app';
 
 import './_app.css';
 
-// const saira = Saira({
-//   weight: ['600', '700'],
-//   style: ['normal'],
-//   subsets: ['latin'],
-// });
+const saira = Saira({
+  weight: ['600', '700'],
+  style: ['normal'],
+  subsets: ['latin'],
+});
 
-// const dm_sans = DM_Sans({
-//   weight: ['400', '700'],
-//   style: ['normal'],
-//   subsets: ['latin'],
-// });
+const dm_sans = DM_Sans({
+  weight: ['400', '700'],
+  style: ['normal'],
+  subsets: ['latin'],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -25,9 +25,9 @@ export default function App({ Component, pageProps }: AppProps) {
         <title>Crosschain Message Dashboard</title>
       </Head>
 
-      {/* <div className={`${saira.className} ${dm_sans.className}`}> */}
-      <Component {...pageProps} />
-      {/* </div> */}
+      <div className={`${saira.className} ${dm_sans.className}`}>
+        <Component {...pageProps} />
+      </div>
     </>
   );
 }
