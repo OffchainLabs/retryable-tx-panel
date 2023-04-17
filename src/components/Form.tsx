86c053a809af0658c2f9a82a1613dc4b12586491
Christophe <contact@christophedeveaux.com>
2023-04-17 21:08:13 +0200

    Add error message for invalid transaction

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -9,14 +9,16 @@ const Form = () => {
   const router = useRouter();
   const tx = pathname.split('/tx/')[1];
   const [value, setValue] = useState(tx ?? '');
+  const [error, setError] = useState<string | null>(null);
 
   const handleSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
 
     if (isValidTxHash(value)) {
+      setError(null);
       router.push(`/tx/${value}`);
-    } else {
-      setValue('');
+    } else if (value) {
+      setError('Invalid address');
       router.push('/');
     }
   };
@@ -24,14 +26,19 @@ const Form = () => {
   return (
     <>
       <form className="form-container" onSubmit={handleSubmit}>
-        <input
-          name="txInput"
-          placeholder="Paste your transaction hash"
-          className="input-style"
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-        />
-        <input type="submit" value="Submit" />
+        <div className="form-inner">
+          <input
+            name="txInput"
+            placeholder="Paste your transaction hash"
+            className="input-style"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+          <input type="submit" value="Submit" />
+        </div>
+        {error && (
+          <div className="error">The transaction hash is not valid</div>
+        )}
       </form>
       <Tooltip id="title-info" place="top" className="tooltip" />
     </>
