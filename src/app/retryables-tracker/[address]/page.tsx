ce73a0fcb8ba1206668e826184db79a192514f19
Christophe <contact@christophedeveaux.com>
2023-05-23 14:56:35 +0200

    Remove redeemed retryables

--- src/app/retryables-tracker/[address]/page.tsx
+++ src/app/retryables-tracker/[address]/page.tsx
@@ -1,7 +1,5 @@
 import Link from 'next/link';
 import { Suspense } from 'react';
-import DepositRetryables from './DepositsRetryables';
-import NonDepositsRetryables from './NonDepositsRetryables';
 import PendingRetryables from './PendingRetryables';
 
 const Page = async ({ params }: { params: { address: string } }) => {
@@ -14,19 +12,7 @@ const Page = async ({ params }: { params: { address: string } }) => {
       <h3>Pending Retryables</h3>
       <Suspense fallback={<div>Loading...</div>}>
         {/* @ts-expect-error Server Component */}
-        <PendingRetryables address={address} limit={10} />
-      </Suspense>
-
-      <h3>Deposit Retryables</h3>
-      <Suspense fallback={<div>Loading deposit retryables...</div>}>
-        {/* @ts-expect-error Server Component */}
-        <DepositRetryables address={address} limit={10} />
-      </Suspense>
-
-      <h3>Non deposits Retryables</h3>
-      <Suspense fallback={<div>Loading non deposit retryables...</div>}>
-        {/* @ts-expect-error Server Component */}
-        <NonDepositsRetryables address={address} limit={10} />
+        <PendingRetryables address={address} />
       </Suspense>
     </>
   );
