4b57e5f0bc5c5640985410950ec180354df4a7cf
Christophe <contact@christophedeveaux.com>
2024-03-04 15:54:59 +0000

    Fix error logging

--- next.config.js
+++ next.config.js
@@ -1,1 +1,12 @@
-module.exports = {};
+module.exports = {
+  webpack: (config, context) => {
+    if (config.plugins) {
+      config.plugins.push(
+        new context.webpack.IgnorePlugin({
+          resourceRegExp: /^(lokijs|pino-pretty|encoding)$/,
+        }),
+      );
+    }
+    return config;
+  },
+};
