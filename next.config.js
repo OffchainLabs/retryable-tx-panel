3b47f5e19b5b3d49984c4912dab593b61d327a14
Christophe <contact@christophedeveaux.com>
2024-03-06 15:54:16 +0000

    Add comments

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  // pino throw error during yarn build,
+  // see https://github.com/WalletConnect/walletconnect-monorepo/issues/1908#issuecomment-1487801131
   webpack: (config, context) => {
     if (config.plugins) {
       config.plugins.push(
