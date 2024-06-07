b02d8aa056a02776411183a9959e67e142ef054f
Christophe <contact@christophedeveaux.com>
2024-06-07 10:20:22 +0000

    refactor: Move subgraph url to .env file

--- src/app/retryables-tracker/[address]/querySubgraph.ts
+++ src/app/retryables-tracker/[address]/querySubgraph.ts
@@ -1,9 +1,22 @@
-export enum subgraphUrl {
-  Retryables = 'https://api.thegraph.com/subgraphs/name/gvladika/arbitrum-retryables',
-  Bridge = 'https://api.thegraph.com/subgraphs/name/gvladika/arb-bridge-eth-nitro',
+const retryablesSubgraphUrl = process.env.RETRYABLES_SUBGRAPH_URL;
+const bridgeSubgraphUrl = process.env.BRIDGE_SUBGRAPH_URL;
+
+if (!retryablesSubgraphUrl || !bridgeSubgraphUrl) {
+  throw new Error('Missing subgraph Urls');
 }
 
-export async function querySubgraph(query: string, url: subgraphUrl) {
+export enum SubgraphQuery {
+  Retryables = 'retryables',
+  Bridge = 'bridge',
+}
+
+const urls = {
+  [SubgraphQuery.Retryables]: retryablesSubgraphUrl,
+  [SubgraphQuery.Bridge]: bridgeSubgraphUrl,
+};
+
+export async function querySubgraph(query: string, type: SubgraphQuery) {
+  const url = urls[type];
   const response = await fetch(url, {
     method: 'POST',
     headers: {
